tellesnobrega/sahara

commit 6894d85bb88b34ec33fc0bece7f356983a0392f2

Enable HDFS NameNode High Availability with HDP 2.0.6 plugin
Extend HDP 2.0.6 plugin to include the setup and configuration of the
HDFS NameNode High Availability after creating and configuring
the cluster.

Change-Id: I32116d42453bd3470b65bf863cf493f27301dab7
Implements: blueprint hdp-plugin-enable-hdfs-ha

sahara/plugins/hdp/versions/version_2_0_6/services.py
@@ -130,10 +130,36 @@ class HdfsService(Service):
         return 'HDFS'
 
     def validate(self, cluster_spec, cluster):
-        # check for a single NAMENODE
-        count = cluster_spec.get_deployed_node_group_count('NAMENODE')
-        if count != 1:
-            raise ex.InvalidComponentCountException('NAMENODE', 1, count)
+        # Check NAMENODE and HDFS HA constraints
+        nn_count = cluster_spec.get_deployed_node_group_count('NAMENODE')
+        jn_count = cluster_spec.get_deployed_node_group_count('JOURNALNODE')
+        zkfc_count = cluster_spec.get_deployed_node_group_count('ZKFC')
+
+        if cluster_spec.is_hdfs_ha_enabled(cluster):
+            if nn_count != 2:
+                raise ex.NameNodeHAConfigurationError(
+                    "Hadoop cluster with HDFS HA enabled requires "
+                    "2 NAMENODE. Actual NAMENODE count is %s" % nn_count)
+            # Check the number of journalnodes
+            if not (jn_count >= 3 and (jn_count % 2 == 1)):
+                raise ex.NameNodeHAConfigurationError(
+                    "JOURNALNODE count should be an odd number "
+                    "greater than or equal 3 for NameNode High Availability. "
+                    "Actual JOURNALNODE count is %s" % jn_count)
+        else:
+            if nn_count != 1:
+                raise ex.InvalidComponentCountException('NAMENODE', 1,
+                                                        nn_count)
+            # make sure that JOURNALNODE is only used when HDFS HA is enabled
+            if jn_count > 0:
+                raise ex.NameNodeHAConfigurationError(
+                    "JOURNALNODE can only be added when "
+                    "NameNode High Availability is enabled.")
+            # make sure that ZKFC is only used when HDFS HA is enabled
+            if zkfc_count > 0:
+                raise ex.NameNodeHAConfigurationError(
+                    "ZKFC can only be added when "
+                    "NameNode High Availability is enabled.")
 
     def finalize_configuration(self, cluster_spec):
         nn_hosts = cluster_spec.determine_component_hosts('NAMENODE')
@@ -630,6 +656,15 @@ class ZookeeperService(Service):
             raise ex.InvalidComponentCountException(
                 'ZOOKEEPER_SERVER', '1+', count)
 
+        # check if HDFS HA is enabled
+        if cluster_spec.is_hdfs_ha_enabled(cluster):
+            # check if we have an odd number of zookeeper_servers > 3
+            if not (count >= 3 and (count % 2 == 1)):
+                raise ex.NameNodeHAConfigurationError(
+                    "ZOOKEEPER_SERVER count should be an odd number "
+                    "greater than 3 for NameNode High Availability. "
+                    "Actual ZOOKEEPER_SERVER count is %s" % count)
+
     def is_mandatory(self):
         return True
 
@@ -1070,6 +1105,17 @@ class HueService(Service):
             r.write_file_to('/etc/hue/conf/hue.ini',
                             hue_ini,
                             True)
+            # update hue.ini if HDFS HA is enabled and restart hadoop-httpfs
+            # /tmp/hueini-hdfsha is written by versionhandler when HDFS is
+            # enabled
+            r.execute_command('[ -f /tmp/hueini-hdfsha ] && sed -i '
+                              '"s/hdfs.*.:8020/hdfs:\\/\\/`cat '
+                              '/tmp/hueini-hdfsha`/g" /etc/hue/conf/hue.ini',
+                              run_as_root=True)
+            r.execute_command('[ -f /tmp/hueini-hdfsha ] && sed -i '
+                              '"s/http.*.\\/webhdfs\\/v1\\//http:\\/\\'
+                              '/localhost:14000\\/webhdfs\\/v1\\//g" '
+                              '/etc/hue/conf/hue.ini', run_as_root=True)
 
             LOG.info(_LI('Uninstalling Shell, if it is installed '
                          'on {0}').format(instance.fqdn()))
@@ -1094,9 +1140,14 @@ class HueService(Service):
             else:
                 cmd = ''
 
-            cmd += '/etc/init.d/hue restart'
+            cmd += 'service hue start'
 
             r.execute_command(cmd, run_as_root=True)
+
+            # start httpfs if HDFS HA is enabled
+            r.execute_command('[ -f /tmp/hueini-hdfsha ] &&'
+                              'service hadoop-httpfs start',
+                              run_as_root=True)
 
     def finalize_configuration(self, cluster_spec):
         # add Hue-specific properties to the core-site file ideally only on

sahara/plugins/exceptions.py
@@ -98,3 +98,18 @@ class HadoopProvisionError(e.SaharaException):
         self.message = self.base_message % message
 
         super(HadoopProvisionError, self).__init__()
+
+
+class NameNodeHAConfigurationError(e.SaharaException):
+    """Exception indicating that hdp-2.0.6 HDFS HA failed.
+
+    A message indicating the reason for failure must be provided.
+    """
+
+    base_message = _("NameNode High Availability: %s")
+
+    def __init__(self, message):
+        self.code = "NAMENODE_HIGHAVAILABILITY_CONFIGURATION_FAILED"
+        self.message = self.base_message % message
+
+        super(NameNodeHAConfigurationError, self).__init__()

sahara/plugins/hdp/ambariplugin.py
@@ -26,6 +26,7 @@ from sahara.plugins.hdp import saharautils as utils
 from sahara.plugins.hdp.versions import versionhandlerfactory as vhf
 from sahara.plugins import provisioning as p
 from sahara.topology import topology_helper as th
+from sahara.utils import general as g
 
 
 conductor = conductor.API
@@ -68,6 +69,34 @@ class AmbariPlugin(p.ProvisioningPluginBase):
         LOG.info(_LI("Install of Hadoop stack successful."))
         # add service urls
         self._set_cluster_info(cluster, cluster_spec)
+
+        # check if HDFS HA is enabled; set it up if so
+        if cluster_spec.is_hdfs_ha_enabled(cluster):
+            cluster = g.change_cluster_status(cluster, "Configuring HA")
+            self.configure_hdfs_ha(cluster)
+
+    def configure_hdfs_ha(self, cluster):
+        version = cluster.hadoop_version
+        handler = self.version_factory.get_version_handler(version)
+
+        cluster_spec = handler.get_cluster_spec(
+            cluster, self._map_to_user_inputs(
+                version, cluster.cluster_configs))
+        hosts = self._get_servers(cluster)
+        ambari_info = self.get_ambari_info(cluster_spec)
+        self.cluster_ambari_mapping[cluster.name] = ambari_info
+        rpm = self._get_rpm_uri(cluster_spec)
+
+        servers = []
+        for host in hosts:
+            host_role = utils.get_host_role(host)
+            servers.append(
+                h.HadoopServer(host, cluster_spec.node_groups[host_role],
+                               ambari_rpm=rpm))
+
+        ambari_client = handler.get_ambari_client()
+        ambari_client.setup_hdfs_ha(cluster_spec, servers, ambari_info,
+                                    cluster.name)
 
     def _get_servers(self, cluster):
         servers = []

sahara/plugins/hdp/clusterspec.py
@@ -112,6 +112,14 @@ class ClusterSpec(object):
 
         return components
 
+    def is_hdfs_ha_enabled(self, cluster):
+        if self.version == '2.0.6':
+            if cluster.cluster_configs.get('HDFSHA', False):
+                if cluster.cluster_configs.HDFSHA.get('hdfs.nnha',
+                                                      False) is True:
+                    return True
+        return False
+
     def _parse_services(self, template_json):
         handler = (vhf.VersionHandlerFactory.get_instance().
                    get_version_handler(self.version))

sahara/plugins/hdp/hadoopserver.py
@@ -198,6 +198,49 @@ class HadoopServer(object):
         # running)
         r.execute_command('ambari-agent restart', run_as_root=True)
 
+    @saharautils.inject_remote('r')
+    def set_namenode_safemode(self, jh, r):
+        r.execute_command("sudo su -l hdfs -c 'JAVA_HOME={0} "
+                          "hdfs dfsadmin -safemode enter'".format(jh),
+                          run_as_root=True)
+
+    @saharautils.inject_remote('r')
+    def save_namenode_namespace(self, jh, r):
+        r.execute_command("sudo su -l hdfs -c 'JAVA_HOME={0} "
+                          "hdfs dfsadmin -saveNamespace'".format(jh),
+                          run_as_root=True)
+
+    @saharautils.inject_remote('r')
+    def initialize_shared_edits(self, jh, r):
+        r.execute_command("sudo su -l hdfs -c 'JAVA_HOME={0} "
+                          "hdfs namenode -initializeSharedEdits'".format(jh),
+                          run_as_root=True)
+
+    @saharautils.inject_remote('r')
+    def format_zookeeper_fc(self, jh, r):
+        r.execute_command("sudo su -l hdfs -c 'JAVA_HOME={0} "
+                          "hdfs zkfc -formatZK'".format(jh),
+                          run_as_root=True)
+
+    @saharautils.inject_remote('r')
+    def bootstrap_standby_namenode(self, jh, r):
+        r.execute_command("sudo su -l hdfs -c 'JAVA_HOME={0} "
+                          "hdfs namenode -bootstrapStandby'".format(jh),
+                          run_as_root=True)
+
+    @saharautils.inject_remote('r')
+    def install_httpfs(self, r):
+        r.execute_command("yum -y install hadoop-httpfs", run_as_root=True)
+
+    @saharautils.inject_remote('r')
+    def start_httpfs(self, r):
+        r.execute_command("service hadoop-httpfs start", run_as_root=True)
+
+    @saharautils.inject_remote('r')
+    def write_hue_temp_file(self, filename, content, r):
+        r.execute_command("echo %s > %s" % (content, filename),
+                          run_as_root=True)
+
     def _log(self, buf):
         LOG.debug(buf)
 

sahara/plugins/hdp/versions/version_2_0_6/resources/default-cluster.template
@@ -44,6 +44,16 @@
           "name" : "HDFS_CLIENT",
           "type" : "CLIENT",
           "cardinality" : "1+"
+        },
+        {
+          "name" : "JOURNALNODE",
+          "type" : "MASTER",
+          "cardinality" : "1+"
+        },
+        {
+          "name" : "ZKFC",
+          "type" : "MASTER",
+          "cardinality" : "1+"
         }
       ],
       "configurations" : [
@@ -1824,6 +1834,12 @@
         { "name": "oozie.service.ProxyUserService.proxyuser.hue.hosts", "value": "*" },
         { "name": "oozie.service.ProxyUserService.proxyuser.hue.groups", "value": "*" }
       ]
+    },
+    {
+      "name" : "hdfsha",
+      "properties" : [
+        { "name": "hdfs.nnha", "value": "false"}
+     ]
     }
   ]
 }

commit 8c2645d8e2585a5d33c0c253213b36e5a6b49fc4

HDP plugin: Fix Bash error when starting Hue
This patch fixes two problems introduced by commit 6894d85, that make
cluster provisioning fail when installing Hue without a namenode in HA.

* Execution of:
    [ -f /tmp/hueini-hdfsha ] && sed -i ...
  always fails when /tmp/hueini-hdfsha does not exist, which puts
  cluster in error. Switching to:
    [ ! -f /tmp/hueini-hdfsha ] || sed -i ...
  fails only when sed fails.

* When running:
    [ -f /tmp/hueini-hdfsha ] && sed ... `cat /tmp/hueini-hdfsha` ...
  the cat command is run before testing the file existence. The special
  quotes (grave accents) need to be escaped.

Change-Id: I26c184f2f4c19f7d084b4a11d5ba439249c5ba92
Closes-Bug: #1429905

sahara/plugins/hdp/versions/version_2_0_6/services.py
@@ -1108,11 +1108,11 @@ class HueService(Service):
             # update hue.ini if HDFS HA is enabled and restart hadoop-httpfs
             # /tmp/hueini-hdfsha is written by versionhandler when HDFS is
             # enabled
-            r.execute_command('[ -f /tmp/hueini-hdfsha ] && sed -i '
+            r.execute_command('[ ! -f /tmp/hueini-hdfsha ] || sed -i '
                               '"s/hdfs.*.:8020/hdfs:\\/\\/`cat '
                               '/tmp/hueini-hdfsha`/g" /etc/hue/conf/hue.ini',
                               run_as_root=True)
-            r.execute_command('[ -f /tmp/hueini-hdfsha ] && sed -i '
+            r.execute_command('[ ! -f /tmp/hueini-hdfsha ] || sed -i '
                               '"s/http.*.\\/webhdfs\\/v1\\//http:\\/\\'
                               '/localhost:14000\\/webhdfs\\/v1\\//g" '
                               '/etc/hue/conf/hue.ini', run_as_root=True)
@@ -1145,7 +1145,7 @@ class HueService(Service):
             r.execute_command(cmd, run_as_root=True)
 
             # start httpfs if HDFS HA is enabled
-            r.execute_command('[ -f /tmp/hueini-hdfsha ] &&'
+            r.execute_command('[ ! -f /tmp/hueini-hdfsha ] || '
                               'service hadoop-httpfs start',
                               run_as_root=True)
 

sahara/utils/ssh_remote.py
@@ -129,6 +129,7 @@ def _read_paramimko_stream(recv_func):
 def _escape_quotes(command):
     command = command.replace('\\', '\\\\')
     command = command.replace('"', '\\"')
+    command = command.replace('`', '\\`')
     return command
 
 